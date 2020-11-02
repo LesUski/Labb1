4a3b7368a964301cca9483fdd9803f30e15b5617
LezMalmo <70897432+LezMalmo@users.noreply.github.com>
2020-11-02 15:54:34 +0100

    Add files via upload

---
+++ src/com/AbstractDataTypes/Main.java
@@ -0,0 +1,67 @@
+package com.AbstractDataTypes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(12);
+        list.add(13);
+        System.out.println("Actual list size: " + list.size());
+        list.add(1,99);
+        System.out.println("================");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("index nr " + i + " : " +  list.get(i));
+        }
+        list.add(55);
+        System.out.println("================");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("index nr " + i + " : " +  list.get(i));
+        }
+        list.remove(2);
+        System.out.println("================");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("index nr " + i + " : " +  list.get(i));
+        }
+
+        System.out.println("List is empty: " + list.empty());
+
+        System.out.println("list contains 0: " + list.contains(0));
+        System.out.println("list contains 99: " + list.contains(99));
+
+        System.out.println("\nStack solution :\n" +
+                "=======================================================");
+        Stack stack = new Stack();
+        stack.add(23);
+        stack.add(32);
+        stack.add(10);
+        stack.push(20);
+        for (int i = 0; i < stack.size(); i++) {
+            System.out.println("Index nr: " + i + stack.get(i));
+        }
+        System.out.println("================");
+        stack.pop();
+        for (int i = 0; i < stack.size(); i++) {
+            System.out.println("Index nr: " + i + stack.get(i));
+        }
+
+        System.out.println("\nQueue solution :\n" +
+                "=======================================================");
+        Queue q = new Queue();
+        q.enqueue(12);
+        q.enqueue(22);
+        q.enqueue(33);
+        for (int i = 0; i < q.size(); i++) {
+            System.out.println("index nr : " + i + " : " +q.get(i));
+        }
+
+        System.out.println("================");
+        System.out.println(q.dequeue());
+        for (int i = 0; i < q.size(); i++) {
+            System.out.println("index nr : " + i + " : " +q.get(i));
+        }
+        System.out.println(q.peek());
+
+    }
+}
---
+++ src/com/AbstractDataTypes/Stack.java
@@ -0,0 +1,19 @@
+package com.AbstractDataTypes;
+
+public class Stack extends LinkedList {
+
+    public void push(Object value) {
+        add(value);
+    }
+
+    public Object pop() {
+        Object temp = get(size() - 1);
+        remove(size() - 1);
+        return temp;
+    }
+
+    public Object peak() {
+        return get(size() - 1);
+    }
+
+}
